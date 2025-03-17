45f321d55d35555b7173074b4cbb42a8f0036d9f
Adrian <adrian.gofa@gmail.com>
2025-03-17 12:18:48 +0100

    React

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.tsx';
 import PaginaPrincipal from './components/PaginaPrincipal.tsx';
 import Footer from './components/Footer.tsx';
+import Esquelas from './components/Esquelas.tsx';
 
 function App() {
   return (
-    <>
-      <Header/>
+    <Router>
+      <Header />
 
-      <PaginaPrincipal/>
-    
-      <Footer/>
-    </>
+      <Routes>
+        <Route path="/" element={<PaginaPrincipal />} />
+        <Route path="/Esquelas" element={<Esquelas />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function Header(){
     return(
         <>
@@ -11,39 +13,39 @@ export default function Header(){
                 
                     <ul className="flex space-x-6 text-gray-700">
                         <li><a href="#" className="hover:text-blue-600">Nosotros</a></li>
-                        <li><a href="#" className="hover:text-blue-600">Esquelas</a></li>
+                        <li><Link to="/Esquelas">Esquelas</Link></li>
                         <li><a href="#" className="hover:text-blue-600">Testamento social</a></li>
                         <li><a href="#" className="hover:text-blue-600">Blog</a></li>
                         <li><a href="#" className="hover:text-blue-600">Comunidad</a></li>
                     </ul>
                     
-                <div className="flex items-center space-x-4">
-                    
-                    <a href="/perfil" className="flex items-center space-x-2"> 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 7.5A3.75 3.75 0 1 1 12 3.75a3.75 3.75 0 0 1 3.75 3.75zM18.75 21a7.5 7.5 0 1 0-13.5 0"></path>
-                    </svg>
-                    </a>
-                
-                    <section className="relative">
-                    <button className="flex items-center space-x-1 p-2 rounded-lg hover:bg-gray-300 focus:outline-none">
-                        <svg className="w-6 h-6 rounded-full" viewBox="0 0 512 512">
-                        <circle cx="256" cy="256" r="256" fill="#ffda44"/>
-                        <path fill="#d80027" d="M0 144h512v224H0z"/>
+                    <div className="flex items-center space-x-4">
+                        
+                        <a href="/perfil" className="flex items-center space-x-2"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 7.5A3.75 3.75 0 1 1 12 3.75a3.75 3.75 0 0 1 3.75 3.75zM18.75 21a7.5 7.5 0 1 0-13.5 0"></path>
                         </svg>
+                        </a>
                     
-                        <span>&#9662;</span> 
-                    </button>
-                    
-                    <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-300 rounded-lg shadow-lg hidden">
-                        <ul className="py-2">
-                        <li><a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Catalán</a></li>
-                        <li><a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Castellano</a></li>
-                        <li><a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Inglés</a></li>
-                        </ul>
+                        <section className="relative">
+                        <button className="flex items-center space-x-1 p-2 rounded-lg hover:bg-gray-300 focus:outline-none">
+                            <svg className="w-6 h-6 rounded-full" viewBox="0 0 512 512">
+                            <circle cx="256" cy="256" r="256" fill="#ffda44"/>
+                            <path fill="#d80027" d="M0 144h512v224H0z"/>
+                            </svg>
+                        
+                            <span>&#9662;</span> 
+                        </button>
+                        
+                        <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-300 rounded-lg shadow-lg hidden">
+                            <ul className="py-2">
+                            <li><a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Catalán</a></li>
+                            <li><a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Castellano</a></li>
+                            <li><a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Inglés</a></li>
+                            </ul>
+                        </div>
+                        </section>
                     </div>
-                    </section>
-                </div>
                 </nav>
             </header>
         </>
